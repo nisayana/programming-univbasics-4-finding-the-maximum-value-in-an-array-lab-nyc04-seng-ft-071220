0022855230baeefb203b23ab8262a8e96c7f090b
Sayana <nisayana23@yandex.ru>
2020-07-20 20:01:01 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-puts array.max
+  count = 0
+  max_num = -1
+  while count < array.length do
+    if max_num < array[count]
+      max_num = array[count]
+    end
+    count +=1
+  end
+  max_num
 end
